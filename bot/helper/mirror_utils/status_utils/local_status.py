f559369fb76e1f3a0e3d16e768e453aa58087b62
raj141414 <148778305+raj141414@users.noreply.github.com>
2024-12-26 10:54:56 +0530

    Create local_status.py

---
+++ bot/helper/mirror_utils/status_utils/local_status.py
@@ -0,0 +1,44 @@
+from bot import LOGGER
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus
+
+
+
+class LocalStatus:
+    def __init__(self, name, size, gid, listener):
+        self.__name = name
+        self.__gid = gid
+        self.__size = size
+        self.__listener = listener
+        self.message = listener.message
+
+    def gid(self):
+        return self.__gid
+
+    def progress(self):
+        return '0'
+
+    def speed(self):
+        return '0'
+
+    def name(self):
+        return self.__name
+
+    def size(self):
+        return get_readable_file_size(self.__size)
+
+    def eta(self):
+        return '-'
+
+    def status(self):
+        return MirrorStatus.STATUS_LOCAL
+
+    def download(self):
+        return self
+
+    async def cancel_download(self):
+        LOGGER.info(f'Cancelling Local Upload: {self.__name}')
+        self.__listener.suproc = 'cancelled'
+        await self.__listener.onUploadError('Local upload stopped by user!')
+
+  def eng(self):
+        return EngineStatus().STATUS_LOCAL
